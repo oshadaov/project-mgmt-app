4ed692562ce27459aafeeb2930c71a956df492bb
oshadaov <oshadaov@gmail.com>
2025-01-26 02:38:23 +0530

    initial commit

---
+++ server/schema/schema.js
@@ -0,0 +1,28 @@
+const {projects, client, clients} = require('../sampleData.js')
+
+const {GraphQLObjectType,GraphQLID,GraphQLString} = require('graphql');
+
+// Client Type
+
+const ClientType = new GraphQLObjectType({
+    name:"Client",
+    fields: ()=>({
+    id: {type:GraphQLID},
+    name :{type:GraphQLString},
+    email:{type:GraphQLString},
+    phone:{type:GraphQLString}
+})
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        client:{
+            type : ClientType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return clients.find(client => client.id === args.id)
+            }
+        }
+    }
+})
